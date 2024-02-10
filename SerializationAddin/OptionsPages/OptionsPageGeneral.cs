3f04433de6c92df08567114af5dc8b96a0c42ffe
Ken Johnson <kenjohnson777@hotmail.com>
2024-02-10 13:32:57 -0500

    Add project files.

---
+++ SerializationAddin/OptionsPages/OptionsPageGeneral.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+
+namespace SerializationAddin.OptionsPages
+{
+  public class OptionsPageGeneral: DialogPage
+  {
+    private string _mySetting = "Default Value";
+
+    public string MySetting
+    {
+      get { return _mySetting; }
+      set { _mySetting = value; }
+    }
+  }
+}
